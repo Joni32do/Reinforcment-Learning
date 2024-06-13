0fba1882fdac22b1ba7117e760747b5abc31c525
st160996 <st160996@stud.uni-stuttgart.de>
2024-06-13 17:32:09 +0200

    Update ex06-plan-eric.py

--- exercises/exercise_06/ex06-plan-eric.py
+++ exercises/exercise_06/ex06-plan-eric.py
@@ -27,49 +27,54 @@ def rollout(env, maxsteps=100):
 def mcts(env, root, maxiter=500):
     """ TODO: Use this function as a starting point for implementing Monte Carlo Tree Search
     """
-    node = root
-
+    max_depth = 0
     # this is an example of how to add nodes to the root for all possible actions:
     root.children = [Node(root, a) for a in range(env.action_space.n)]
 
     for i in range(maxiter):
         state = copy.deepcopy(env)
         G = 0.
-
+        node  = root
         # TODO: traverse the tree using an epsilon greedy tree policy
 
-        if np.random.random() > 0.2:
-            node = np.argmax(node.children.reward)
+        """     while node.children:            #going to the bottom of the tree
+            if np.random.random() > 0.5:
+                node = max( node.children, key = lambda x: x.sum_value)
+            else:
+                node = random.choice(root.children)
+        """
+        while node.children:                #going to the bottom of the tree
+            values = [c.sum_value/c.visits if c.visits > 0 else 0 for c in node.children] # TODO: Vectorize with np
+            bestchild = node.children[np.argmax(values)]
+            # epsilon greedy
+            if random.random() < 0.5:
+                node = random.choice(node.children)
+            else:
+                node = bestchild
+
+        #Expansion of tree
+        if node.visits>1:
+            node.children = [Node(node, a) for a in range(env.action_space.n)]
+
+        #Choose random action and take it
+            node = random.choice(node.children)
             _, reward, terminal, _ = env.step(node.action)
-        else:
-            node = random.choice(root.children)
-            _, reward, terminal, _ = env.step(node.action)
 
-        G += reward
+            G += reward
 
-        # TODO: Expansion of tree
-        for action in env.action_space:
-            node.children.append(Node(parent=node, action = action))
+            if not terminal:
+                G += rollout(state)
 
-        # This performs a rollout (Simulation):
-
-        action = env.action_space.sample()
-        _, reward, terminal, _ = env.step(action)
-
-        if not terminal:
-            G += rollout(state)
 
         # TODO: update all visited nodes in the tree
-
-        while node is not root
-            node.return = G
+        depth = 0
+        while node is not root:
+            node.visits += 1
+            node.sum_value += G
             node = node.parent
-
-        # This updates values for the current node:
-        node.visits += 1
-        node.sum_value += G
-
-
+            depth += 1
+        if depth > max_depth: max_depth = depth
+    print("max_depth:", max_depth)
 
 
 def main():
@@ -85,7 +90,7 @@ def main():
         while not terminal:
             env.render()
             mcts(env, root)  # expand tree from root node using mcts
-            values = [c.sum_value/c.visits for c in root.children]  # calculate values for child actions
+            values = [c.sum_value/c.visits if c.visits > 0 else 0 for c in root.children]  # calculate values for child actions
             bestchild = root.children[np.argmax(values)]  # select the best child
             _, reward, terminal, _ = env.step(bestchild.action) # perform action for child
             root = bestchild  # use the best child as next root
